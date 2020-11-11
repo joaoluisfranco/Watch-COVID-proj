706e1b6d8750b3680fac528e136dca38f5c48bc0
SK0P3iN <juan_silva1997@live.com.pt>
2020-11-11 23:11:47 +0000

    Notification implementation

--- app/src/main/java/com/example/watchcovid/MainActivity.java
+++ app/src/main/java/com/example/watchcovid/MainActivity.java
@@ -1,21 +1,100 @@
 package com.example.watchcovid;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    // private TextView mTextView;
+    public static String id = "test_channel_01";
+    int notificationID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        //mTextView = (TextView) findViewById(R.id.text);
 
         // Enables Always-on
         setAmbientEnabled();
+
+        addbuttonNoti();
+
+        createchannel();
+    }
+
+    void addbuttonNoti() {
+
+        // we are going to add an intent to open the camera here.
+        //Intent cameraIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        PendingIntent cameraPendingIntent =
+                PendingIntent.getActivity(this, 0, cameraIntent, 0);
+
+        NotificationCompat.Action.WearableExtender inlineActionForWear2 =
+                new NotificationCompat.Action.WearableExtender()
+                        .setHintDisplayActionInline(true)
+                        .setHintLaunchesActivity(true);
+
+        // Add an action to allow replies.
+        NotificationCompat.Action pictureAction =
+                new NotificationCompat.Action.Builder(
+                        R.mipmap.ic_launcher,
+                        //R.drawable.ic_action_time,
+                        "Lavei :)",
+                        cameraPendingIntent)
+                        .extend(inlineActionForWear2)
+                        .build();
+
+        //Now create the notification.  We must use the NotificationCompat or it will not work on the wearable.
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, id)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Lava as mãos!!")
+                        .setContentText("Confirma que lavaste seu porco.")
+                        .setChannelId(id)
+                        .addAction(pictureAction);
+
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationID, notificationBuilder.build());
+        notificationID++;
+
+    }
+
+
+    private void createchannel() {
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel mChannel = new NotificationChannel(id,
+                getString(R.string.channel_name),  //name of the channel
+                NotificationManager.IMPORTANCE_HIGH);   //importance level
+        //important level: default is is high on the phone.  high is urgent on the phone.  low is medium, so none is low?
+        // Configure the notification channel.
+        mChannel.setDescription(getString(R.string.channel_description));
+        mChannel.enableLights(true);
+        //Sets the notification light color for notifications posted to this channel, if the device supports this feature.
+        mChannel.setLightColor(Color.RED);
+        mChannel.enableVibration(true);
+        mChannel.setShowBadge(true);
+        mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        nm.createNotificationChannel(mChannel);
+
     }
 }
